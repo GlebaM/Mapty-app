1caa2317a3bef9167532e11bcd05feb76f2a564e
Michał Gleba <GlebaM@users.noreply.github.com>
2021-08-11 18:16:24 +0200

    Leaflet map applied

--- script.js
+++ script.js
@@ -10,3 +10,34 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  const negativeNavigCBack = function (any) {
+    const { message } = any;
+    alert(`Can not find location. ${message}`);
+    //   console.log(any, message);
+  };
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    console.log(position);
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(latitude, longitude);
+    const coords = [latitude, longitude];
+    console.log(`https://www.google.pl/maps/@${latitude},${longitude}z`);
+    console.log(`Latitude: ${latitude}`);
+    console.log(`Longitude: ${longitude}`);
+
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup(`This is where I am.<br> Nice isn't it?!`)
+      .openPopup();
+  }, negativeNavigCBack);
+}
